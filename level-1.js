220bcee8c31340a7162a1d4ba9ccd900a7ce9072
SubatYucel <subat.yucel1@gmail.com>
2024-07-16 20:53:38 +0300

    day-3 level-1 done

--- level-1.js
+++ level-1.js
@@ -1,83 +1,44 @@
-//1- Declare a variable named challenge and assign it to an initial value '30 Days Of JavaScript'.
-let challenge = "30 Days Of JavaScript";
+//1- Declare firstName, lastName, country, city, age, isMarried, year variable and assign value to it and use the typeof operator to check different data types.
+let firstName = "subat";
+lastName = "Yucel";
+age = 22;
+isMarried = false;
+console.log(typeof firstName, typeof age, typeof isMarried);
 
-//2- Print the string on the browser console using console.log()
-console.log(challenge);
+//2- Check if type of '10' is equal to 10
+console.log(typeof "10" == typeof 10);
 
-//3- Print the length of the string on the browser console using console.log()
-console.log(challenge.length);
+//3- Check if parseInt('9.8') is equal to 10
+console.log(parseInt("9.8") == 10);
 
-//4- Change all the string characters to capital letters using toUpperCase() method
-console.log(challenge.toUpperCase());
+/*4- Boolean value is either true or false.
+      a- Write a JavaScript statement which provide truthy value.
+      b- Write a JavaScript statement which provide falsly value.
+*/
 
-//5- Change all the string characters to lowercase letters using toLowerCase() method
-console.log(challenge.toLowerCase());
+console.log(4 > 3);
+console.log(4 < 3);
 
-//6- Cut (slice) out the first word of the string using substr() or substring() method
-console.log(challenge.substr(0, 2));
-console.log(challenge.substring(0, 2));
+//5* Use the Date object to do the following activities
+let date = new Date();
 
-//7- Slice out the phrase Days Of JavaScript from 30 Days Of JavaScript.
-console.log(challenge.slice(3, challenge.length));
+//What is the year today?
+console.log(date.getFullYear());
 
-//8- Check if the string contains a word Script using includes() method
-console.log(challenge.includes("Script"));
+//What is the month today as a number?
+console.log(date.getMonth());
 
-//9- Split the string into an array using split() method
-console.log(challenge.split());
+//What is the date today?
+console.log(date.getDate());
 
-//10- Split the string 30 Days Of JavaScript at the space using split() method
-console.log(challenge.split(" "));
+//What is the day today as a number?
+console.log(date.getDay());
 
-//11- 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon' split the string at the comma and change it to an array.
-let companies = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon";
-console.log(companies.split(","));
+//What is the hours now?
+console.log(date.getHours());
 
-//12- Change 30 Days Of JavaScript to 30 Days Of Python using replace() method.
-console.log(challenge.replace("JavaScript", "Pyhton"));
+//What is the minutes now?
+console.log(date.getMinutes());
 
-//13- What is character at index 15 in '30 Days Of JavaScript' string? Use charAt() method.
-console.log(challenge.charAt(15));
-
-//14- What is the character code of J in '30 Days Of JavaScript' string using charCodeAt()
-let indexJ = challenge.indexOf("J");
-console.log(challenge.charCodeAt(indexJ));
-
-//15- Use indexOf to determine the position of the first occurrence of "a" in 30 Days Of JavaScript
-console.log(challenge.indexOf("a"));
-
-//16- Use lastIndexOf to determine the position of the last occurrence of "a" in 30 Days Of JavaScript.
-console.log(challenge.lastIndexOf("a"));
-
-//17- Use indexOf to find the position of the first occurrence of the word "because" in the following sentence:'You cannot end a sentence with because because because is a conjunction'
-let sentence =
-  "You cannot end a sentence with because because because is a conjunction";
-console.log(sentence.indexOf("because"));
-
-//18-Use lastIndexOf to find the position of the last occurrence of the word "because"in the following sentence:'You cannot end a sentence with because because because is a conjunction'
-console.log(sentence.lastIndexOf("because"));
-
-//19- Use search to find the position of the first occurrence of the word "because" in the following sentence:'You cannot end a sentence with because because because is a conjunction'
-console.log(sentence.search("because"));
-
-//20- Use trim() to remove any trailing whitespace at the beginning and the end of a string.E.g ' 30 Days Of JavaScript '.
-challenge = " 30 Days Of JavaScript ";
-challenge = challenge.trim();
-console.log(challenge);
-
-//21- Use startsWith() method with the string 30 Days Of JavaScript and make the result true
-console.log(challenge.startsWith("30"));
-
-//22- Use endsWith() method with the string 30 Days Of JavaScript and make the result true
-console.log(challenge.endsWith("JavaScript"));
-
-//23- Use match() method to find all the a's in 30 Days Of JavaScript
-console.log(challenge.match("a"));
-
-//24- Use concat() and merge '30 Days of' and 'JavaScript' to a single string, '30 Days Of JavaScript'
-let word1 = "30 Days of";
-let word2 = "JavaScript";
-console.log(word1.concat(word2));
-
-//25- Use repeat() method to print 30 Days Of JavaScript 2 times
-console.log(challenge.repeat(2));
+//Find out the numbers of seconds elapsed from January 1, 1970 to now.
+console.log(date.getTime());
